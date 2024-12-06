f62b79a42896fac2e8b508e413c460a88b54212b
izzuddin12 <izzuddinfaiz6@gmail.com>
2024-12-06 23:17:39 +0700

    update name & group name

--- src/CellStatus.java
+++ src/CellStatus.java
@@ -1,4 +1,14 @@
 /**
+ * ES234317-Algorithm and Data Structures
+ * Semester Ganjil, 2024/2025
+ * Group Capstone Project
+ * Group #11
+ * 1 - 5026231018 - Izzudin Hamadi Faiz
+ * 2 - 5026231091 - Bagas Rafi Dewantara
+ * 3 - 5026231116 - I Putu Febryan Khrisyantara
+ */
+
+/**
  * An enumeration of constants to represent the status
  * of each cell.
  */
--- src/Puzzle.java
+++ src/Puzzle.java
@@ -1,4 +1,14 @@
 /**
+ * ES234317-Algorithm and Data Structures
+ * Semester Ganjil, 2024/2025
+ * Group Capstone Project
+ * Group #11
+ * 1 - 5026231018 - Izzudin Hamadi Faiz
+ * 2 - 5026231091 - Bagas Rafi Dewantara
+ * 3 - 5026231116 - I Putu Febryan Khrisyantara
+ */
+
+/**
  * The Sudoku number puzzle to be solved
  */
 public class Puzzle {
